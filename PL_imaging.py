d86dcdd65efdd8bf8d479f27f6e7a67715fa2d75
Yang <yangchemoffice@gmail.com>
2025-12-09 10:50:09 -0500

    debug

--- PL_imaging.py
+++ PL_imaging.py
@@ -435,7 +435,7 @@ hyb.check_make_dir(dir_txtsave)
 hyb.save_combined_matrix(data, dt, dx, f"{dir_txtsave}\\{short_name}_data_raw.txt", notice=True)    # only proc notice for the 1st save.
 hyb.save_combined_matrix(data_normall, dt, dx, f"{dir_txtsave}\\{short_name}_data_normall.txt", notice=False)
 hyb.save_combined_matrix(data_norm_t, dt, dx, f"{dir_txtsave}\\{short_name}_data_normt.txt", notice=False)
-hyb.save_combined_matrix(data.T, dx, dt, f"{dir_txtsave}\\{short_name}_data_raw_T.txt", notice=True)
+hyb.save_combined_matrix(data.T, dx, dt, f"{dir_txtsave}\\{short_name}_data_raw_T.txt", notice=False)
 hyb.save_combined_matrix(data_normall.T, dx, dt, f"{dir_txtsave}\\{short_name}_data_normall_T.txt", notice=False)
 hyb.save_combined_matrix(data_norm_t.T, dx, dt, f"{dir_txtsave}\\{short_name}_data_normt_T.txt", notice=False)
 hyb.save_combined_matrix(xfit_data, dt, dx, f"{dir_txtsave}\\{short_name}_fitted_data.txt", notice=False)
